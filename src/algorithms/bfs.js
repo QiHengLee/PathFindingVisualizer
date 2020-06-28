5e5f6e2dcf7f27cdea2b764ff3ca37a2707c5ba7
QiHengLee <57242546+QiHengLee@users.noreply.github.com>
2020-06-28 20:09:49 +0800

    Finish BFS algorithm

--- src/algorithms/bfs.js
+++ src/algorithms/bfs.js
@@ -1,3 +1,47 @@
-function bfs() {
-    return
-}
+export const bfs = (grid, startNode, finishNode) => {
+    var nodeOrder = []
+    var queue = []
+    queue.push(startNode)
+    startNode.isVisited = true;
+    
+    while(queue.length > 0) {
+        var node = queue.shift();
+        var neighbours = getNeighbors(node, grid);
+        for (const neighbour of neighbours) {
+            if (!neighbour.isVisited && !neighbour.isWall) {
+                neighbour.previousNode = node;
+                neighbour.isVisited = true;
+                queue.push(neighbour);
+                nodeOrder.push(neighbour);
+                if (
+                    neighbour.col === finishNode.col &&
+                    neighbour.row === finishNode.row
+                ) {
+                    return nodeOrder;
+                }
+            }
+        }
+    }
+    console.log(nodeOrder)
+    return nodeOrder;
+  };
+
+  function getNeighbors(node, grid) {
+    const neighbours = [];
+    const { row, col } = node;
+    if (row > 0) {
+      neighbours.push(grid[row - 1][col]);
+    }
+    if (col < grid[0].length - 1) {
+      neighbours.push(grid[row][col + 1]);
+    }
+    if (row < grid.length - 1) {
+      neighbours.push(grid[row + 1][col]);
+    }
+    if (col > 0) {
+      neighbours.push(grid[row][col - 1]);
+    }
+  
+    return neighbours;
+  }
+  
