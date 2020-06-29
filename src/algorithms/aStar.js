98f199e6fa5339152b6f57646cd974ce5615a608
QiHengLee <57242546+QiHengLee@users.noreply.github.com>
2020-06-29 21:51:31 +0800

    start A* algo

---
+++ src/algorithms/aStar.js
@@ -0,0 +1,25 @@
+export const aStar = (grid, startNode, finishNode) => {
+    var nodeOrder = [];
+  
+    return nodeOrder;
+  };
+  
+  function getNeighbors(node, grid) {
+    const neighbours = [];
+    const { row, col } = node;
+    if (row > 0) {
+      neighbours.push(grid[row - 1][col]);
+    }
+    if (col < grid[0].length - 1) {
+      neighbours.push(grid[row][col + 1]);
+    }
+    if (row < grid.length - 1) {
+      neighbours.push(grid[row + 1][col]);
+    }
+    if (col > 0) {
+      neighbours.push(grid[row][col - 1]);
+    }
+  
+    return neighbours;
+  }
+  
